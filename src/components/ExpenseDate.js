698ad9e52f6e78af985bbe5eaf10c28328c92e06
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-06 21:35:03 -0700

    Styesheet for expense date.

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
-   const month = props.date.toLocaleString("en-us", { month: "short" });
+   const month = props.date.toLocaleString("en-us", { month: "long" });
    const day = props.date.toLocaleString("en-us", { day: "2-digit" });
    const year = props.date.getFullYear();
 
    return (
-      <div>
-         <div>a{month}</div>
-         <div>{year}</div>
-         <div>{day}</div>
+      <div className="expense-date">
+         <div className="expense-date__month">{month}</div>
+         <div className="expense-date__year">{year}</div>
+         <div className="expense-date__day">{day}</div>
       </div>
    );
 }
