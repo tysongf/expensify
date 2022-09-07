c55ad64fda605f3ac15685bb074e16d2c5fb0405
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-06 21:03:25 -0700

    Added the ExpenseItem component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+   return (
+      <div>
+         <h2>Let's get started!</h2>
+         <p>I 💖 React</p>
+
+         <ExpenseItem />
+      </div>
+   );
 }
 
 export default App;
