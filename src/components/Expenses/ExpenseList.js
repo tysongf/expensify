9c79fb6aabdb58ef7f10b88a467a547be97ad562
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-08 13:27:50 -0700

    render expense list dynamically using array map.

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -17,26 +17,13 @@ const ExpenseList = (props) => {
             selected={yearFilter}
             onExpenseFilter={handleExpenseFilter}
          />
-         <ExpenseItem
-            title={props.expenses[0].title}
-            amount={props.expenses[0].amount}
-            date={props.expenses[0].date}
-         />
-         <ExpenseItem
-            title={props.expenses[1].title}
-            amount={props.expenses[1].amount}
-            date={props.expenses[1].date}
-         />
-         <ExpenseItem
-            title={props.expenses[2].title}
-            amount={props.expenses[2].amount}
-            date={props.expenses[2].date}
-         />
-         <ExpenseItem
-            title={props.expenses[3].title}
-            amount={props.expenses[3].amount}
-            date={props.expenses[3].date}
-         />
+         {props.expenses.map((expense) => (
+            <ExpenseItem
+               title={expense.title}
+               amount={expense.amount}
+               date={expense.date}
+            />
+         ))}
       </Card>
    );
 };
