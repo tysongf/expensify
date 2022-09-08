39ece879c5fe17584cf4e028193d6991d672f5ae
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-08 00:36:09 -0700

    Add year filter which doesn't do much atm.

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,10 +1,22 @@
+import { React, useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./ExpenseList.css";
+import ExpenseFilter from "./ExpenseFilter";
 
 const ExpenseList = (props) => {
+   const [yearFilter, setYearFilter] = useState("2020");
+
+   const handleExpenseFilter = (year) => {
+      setYearFilter(year);
+   };
+
    return (
-      <Card className={"expense-list " + props.className}>
+      <Card className="expense-list">
+         <ExpenseFilter
+            selected={yearFilter}
+            onExpenseFilter={handleExpenseFilter}
+         />
          <ExpenseItem
             title={props.expenses[0].title}
             amount={props.expenses[0].amount}
